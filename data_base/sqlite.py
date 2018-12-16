f7835ba5e5075e4febbbc682dc05d8980c03f12e
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2018-12-16 16:46:15 +0300

    data_base: remove extra methods from sqlite.py

--- data_base/sqlite.py
+++ data_base/sqlite.py
@@ -7,22 +7,6 @@ class SQLighter:
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-    def select_all(self):
-        """ Получаем все строки """
-        with self.connection:
-            return self.cursor.execute('SELECT * FROM music').fetchall()
-
-    def select_single(self, rownum):
-        """ Получаем одну строку с номером rownum """
-        with self.connection:
-            return self.cursor.execute('SELECT * FROM music WHERE id = ?', (rownum,)).fetchall()[0]
-
-    def count_rows(self):
-        """ Считаем количество строк """
-        with self.connection:
-            result = self.cursor.execute('SELECT * FROM music').fetchall()
-            return len(result)
-
     def execute(self, request):
         print("request:", request)
         with self.connection:
