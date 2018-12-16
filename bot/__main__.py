e8872311bc8d5c75d998355f3f8f11890c04b484
gitnail <n.nail15@mail.ru>
2018-12-16 18:39:45 +0300

    fix

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,60 +1,52 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-import config
 import datetime
-import os
-import sys
 import telebot
 from telebot import types
 
-sys.path.append("../data_base")
-
 from commands import *
-from sqlite import SQLighter
 
-bot = telebot.TeleBot(config.token)
-
-DB_PATH = os.path.join("../data_base", config.database_name)
-    
-worker = {}
+BOT = telebot.TeleBot(config.token)
+ 
+WORKER = {}
 
 
-@bot.message_handler(commands=["start"])
+@BOT.message_handler(commands=["start"])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Welcome to accidents DB client using SQLite DB")
+    BOT.send_message(message.chat.id, "Welcome to accidents DB client using SQLite DB")
 
 
-@bot.message_handler(commands=["help"])
+@BOT.message_handler(commands=["help"])
 def handle_help(message):
-    bot.send_message(message.chat.id, "TODO")
+    BOT.send_message(message.chat.id, "TODO")
 
 
-@bot.message_handler(commands=["get_by_date"])
+@BOT.message_handler(commands=["get_by_date"])
 def handle_get_by_date(message):
     markup = types.ForceReply(selective=False)
-    worker.update({message.from_user.id: get_by_date})
-    bot.send_message(message.chat.id, "Please enter a date", reply_markup=markup)
+    WORKER.update({message.from_user.id: get_by_date})
+    BOT.send_message(message.chat.id, "Please enter a date", reply_markup=markup)
 
 
-@bot.message_handler(commands=["execute"])
+@BOT.message_handler(commands=["execute"])
 def handle_execute(message):
     markup = types.ForceReply(selective=False)
-    worker.update({message.from_user.id: execute})
-    bot.send_message(message.chat.id, "Please enter an SQL command to execute", reply_markup=markup)
+    WORKER.update({message.from_user.id: execute})
+    BOT.send_message(message.chat.id, "Please enter an SQL command to execute", reply_markup=markup)
 
 
-@bot.message_handler(content_types=["text"])
+@BOT.message_handler(content_types=["text"])
 def handle_text(message):
     uid = message.from_user.id
-    if uid in worker:
-        worker[uid](message)
+    if uid in WORKER:
+        WORKER[uid](message, BOT)
         try:
-            del worker[uid]
+            del WORKER[uid]
         except:
             pass
     else:
-        bot.send_message(message.chat.id, "Id dont' undestand :(")
+        BOT.send_message(message.chat.id, "Id dont' undestand :(")
 
 
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
+    BOT.polling(none_stop=True)
--- bot/commands.py
+++ bot/commands.py
@@ -1,29 +1,34 @@
-def proceess_db_result(res):
-    res = res.strip()
-    if not res:
-        res = "<empty_reply>"
+import os
+import config
+import sys
+sys.path.append("../data_base")
+
+from sqlite import SQLighter
+
+DB_PATH = os.path.join("../data_base", config.database_name)
 
 
-def do_safe(func, message):
+def do_safe(func, bot, message):
     try:
         func(message)
     except Exception as ex:
         bot.send_message(message.chat.id, "Error: '{}'".format(ex))
 
 
-def execute(message):
+def execute(message, bot):
     def action(message):
         client = SQLighter(DB_PATH)
         res = client.execute(message.text)
-        proceess_db_result(res)
+        if not res:
+            res = "<empty_reply>"
         bot.send_message(message.chat.id, res)
 
-    do_safe(action, message)
+    do_safe(action, bot, message)
 
 
-def get_by_date(message):
+def get_by_date(message, bot):
     def action(message):
         date = datetime.datetime.strptime(message.text, "%Y-%M-%d")
         bot.send_message(message.chat.id, "Date format is correct")
     
-    do_safe(action, message)
+    do_safe(action, bot, message)
